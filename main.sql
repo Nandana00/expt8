d6871204e88072ccc0b88a9fd230fdeb86f2be9e
Nandana00 <68513830+Nandana00@users.noreply.github.com>
2021-01-08 17:42:10 +0530

    Add files via upload

---
+++ main.sql
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                        Online SQLite Query Runner.
+                Code, Compile, Run and Debug SQLite query online.
+Write your query in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+
+/* Enter your sql queries here */
+CREATE TABLE EMPLOYEE(
+CODE VARCHAR(20),
+NAME CHAR(20),
+DOB DATE,
+DESIGNATION VARCHAR(20),
+SALARY int(20)
+)
+
+SELECT * FROM EMPLOYEE
+INSERT INTO EMPLOYEE VALUES
+('A1','EMIL','12-01-1995','MD',40000),
+('A2','DAVID','04-12-1993','HR',25000),
+('A3','AISHA','11-04-1999','MANAGER',15000),
+('A4','MEENA','27-07-1994','MANAGER',15000);
+
+SELECT SUM(Salary) FROM EMPLOYEE
+WHERE Designation='clerk';
+
+SELECT MAX(Salary) FROM Employee;
+
+SELECT AVG(Salary) FROM Employee;
+
+SELECT MIN(Salary) FROM Employee;
+
+SELECT COUNT(NAME) FROM Employee
